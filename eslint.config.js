7d116795c8fbd30c78214a668b3a49643b24e203
ek-ole <ek_34@mail.ru>
2025-05-01 13:09:31 +0300

    feat: (ECA-12) set up ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,55 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import testingLibrary from 'eslint-plugin-testing-library'
+import jestDom from 'eslint-plugin-jest-dom'
+import vitest from "@vitest/eslint-plugin"
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended, 
+      ...tseslint.configs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      reactHooks.configs['recommended-latest'],
+      jsxA11y.flatConfigs.recommended,
+      eslintConfigPrettier,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'testing-library': testingLibrary,
+      'jest-dom': jestDom,
+      'vitest': vitest,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
-        'warn',
+       'warn',
         { allowConstantExport: true },
       ],
+      ...testingLibrary.configs.react.rules,
+      ...jestDom.configs.recommended.rules,
+      ...vitest.configs.recommended.rules,
     },
+
   },
+  {
+    "settings": {
+      "react": {
+        "version": "detect",
+      }
+    }
+  }
 )
