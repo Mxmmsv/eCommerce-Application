b86074dea4edbb7f36374761cb8f6f4573f008e6
ek-ole <ek_34@mail.ru>
2025-05-03 14:31:09 +0300

    refactor: add button to App.tsx with tailwind & shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-300">
+      <Button variant="outline">Click</Button>
+    </div>
+  );
 }
 
 export default App;
