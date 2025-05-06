21d5a8f34f66d544ea22e484669d3bef1bd81b0b
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-06 14:05:15 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
-import { Button } from '@/components/ui/button';
+import AboutUs from '@/components/layout/AboutUs-layout';
+import Basket from '@/components/layout/Basket-layout';
+import Catalog from '@/components/layout/Catalog-layout';
+import Login from '@/components/layout/Login-layout';
+import Main from '@/components/layout/Main-layout';
+import ProductDetail from '@/components/layout/ProductDetail-layout';
+import Registration from '@/components/layout/Registration-layout';
+import UserProfile from '@/components/layout/UserProfile-layout';
+import Navbar from '@/components/ui/navBar/NavBar';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-300">
-      <div>Counter: {counter}</div>
-      <Button variant="outline" onClick={() => setCounter((prev) => prev + 1)}>
-        Click
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/catalog/:id" element={<ProductDetail />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
