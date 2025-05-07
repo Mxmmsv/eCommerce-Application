1c1bad176f2c33b8fbe4fb0b8fec7220cfebf4e0
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-07 14:09:41 +0300

    feat(ECA-31): add login page with route to main page and registration page

--- src/components/layout/Login-layout.tsx
+++ src/components/layout/Login-layout.tsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router';
+
+import { LoginForm } from '@/components/ui/loginForm/login-form';
+
 export default function LoginPage() {
-  return <h1>Login Page</h1>;
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link to="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-10 w-10 items-center justify-center">
+            <img src="logo.svg" alt="logo" />
+          </div>
+          Poster Store
+        </Link>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
