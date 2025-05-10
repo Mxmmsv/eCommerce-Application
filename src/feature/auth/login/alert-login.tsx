033259e6c3a69795f6ca44197b9e6b358cc03e0b
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-10 13:50:36 +0300

    feat: (ECA-27) implement alert for failed auth

--- src/feature/auth/login/alert-login.tsx
+++ src/feature/auth/login/alert-login.tsx
@@ -2,12 +2,12 @@ import { Terminal } from 'lucide-react';
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
-export function AlertDemo() {
+export function AlertFailedLogin() {
   return (
     <Alert variant="destructive">
       <Terminal className="h-4 w-4" />
-      <AlertTitle>Ошибка входа!</AlertTitle>
-      <AlertDescription>Проверьте email и пароль и попробуйте снова.</AlertDescription>
+      <AlertTitle>Login error!</AlertTitle>
+      <AlertDescription>Please check your email and password and try again.</AlertDescription>
     </Alert>
   );
 }
