a806f2b9ebf3de5f7c390fa33566a5e5811df155
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-10 14:57:27 +0300

    feat: (ECA-30) implement customer acsess token

--- src/feature/auth/login/handle-login.ts
+++ src/feature/auth/login/handle-login.ts
@@ -2,15 +2,10 @@ import { loginCustomerWithMail } from '@/feature/api/api-login-email';
 import tokenCache from '@/feature/api/api-token-store';
 
 export const handleLogin = async (email: string, password: string) => {
-  try {
-    const response = await loginCustomerWithMail(email, password);
-    const token = tokenCache.get().token;
-    if (token) {
-      localStorage.setItem('accessToken', token);
-    }
-    console.log(`token: ${token}`);
-    console.log(response.body.customer);
-  } catch (error) {
-    console.error('Ошибка логина', error);
+  await loginCustomerWithMail(email, password);
+  const token = tokenCache.get().token;
+  if (!token) {
+    throw new Error('Token not found');
   }
+  localStorage.setItem('accessToken', token);
 };
