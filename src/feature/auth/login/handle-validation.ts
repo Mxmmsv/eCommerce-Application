41ccff077a44a7fa20461412e9aeb1c16c2aa118
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-13 00:05:19 +0300

    refactor: remove haddog in email pipeline, change direction in password pipeline

--- src/feature/auth/login/handle-validation.ts
+++ src/feature/auth/login/handle-validation.ts
@@ -9,17 +9,16 @@ const hasUppercase = regex(/[A-Z]/, 'Must contain at least one uppercase letter'
 const hasLowercase = regex(/[a-z]/, 'Must contain at least one lowercase letter');
 const hasDigit = regex(/[0-9]/, 'Must contain at least one digit');
 const hasSpecialChar = regex(/[!@#$%^&*]/, 'Must contain at least one special character');
-const hasDogMail = regex(/[@]/, `Must contain an '@' symbol separating local part and domain name`);
 
-export const formSchema = object({
-  email: pipe(string(), noEdgeWhitespace, hasDogMail, email('Please enter a valid email address')),
+export const formLoginSchema = object({
+  email: pipe(string(), noEdgeWhitespace, email('Please enter a valid email address')),
   password: pipe(
     string(),
     noEdgeWhitespace,
-    minLength(8, 'Password must be at least 8 characters long'),
     hasUppercase,
     hasLowercase,
     hasDigit,
     hasSpecialChar,
+    minLength(8, 'Password must be at least 8 characters long'),
   ),
 });
