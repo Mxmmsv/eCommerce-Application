b6e3b1459d2da5a4b5d9788d3d10b82c71cb7dfd
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-13 23:21:47 +0300

    feat: add draft of pages

---
+++ src/pages/login-page.tsx
@@ -0,0 +1,10 @@
+import { Baby } from 'lucide-react';
+
+export default function LoginPage() {
+  return (
+    <div className="mt-4 flex flex-col items-center justify-center">
+      <h1>Login Page</h1>
+      <Baby color="green" size={60} />
+    </div>
+  );
+}
---
+++ src/pages/registration-page.tsx
@@ -0,0 +1,10 @@
+import { UsersRound } from 'lucide-react';
+
+export default function RegistrationPage() {
+  return (
+    <div className="mt-4 flex flex-col items-center justify-center">
+      <h1>Registration Page</h1>
+      <UsersRound color="red" size={60} data-testid="users-icon" />
+    </div>
+  );
+}
