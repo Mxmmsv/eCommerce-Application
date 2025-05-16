cabf0f11b225547b2e68201c6881fab5f595ff75
Mxmmsv <61119540+Mxmmsv@users.noreply.github.com>
2025-05-16 22:53:30 +0300

    feat: Implement theme-provider
    * chore: install shadcn toggle
    
    * feat: (ECA-49) implement theme provider
    
    * feat: (ECA-49) add theme provide and close button for toaster in app.tsx
    
    * refactor: (ECA-49) change import for useTheme, remove default theme
    
    * feat: add toggle ui
    
    * feat: (ECA-49) create theme toggler and add it to the header
    
    * feat: add background color for logo

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,9 +1,10 @@
-import { useTheme } from 'next-themes';
 import type { ToasterProps } from 'sonner';
 import { Toaster as Sonner } from 'sonner';
 
+import { useTheme } from '@/components/ui/theme-provider';
+
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = 'dark' } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <Sonner
