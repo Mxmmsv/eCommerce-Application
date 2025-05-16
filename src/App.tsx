caff66c18edc9fc7b4781b3e516370f10c82a613
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-16 20:55:22 +0300

    feat: (ECA-49) add theme provide and close button for toaster in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,29 @@ import ProductDetail from '@/components/page/product-detail-page';
 import Registration from '@/components/page/registration-page';
 import UserProfile from '@/components/page/user-profile-page';
 import { Toaster } from '@/components/ui/sonner';
+import { ThemeProvider } from '@/components/ui/theme-provider';
 import { AuthProvider } from '@/feature/auth/login/auth-provider';
 import Navbar from '@/feature/header/NavBar';
-
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/catalog/:id" element={<ProductDetail />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/basket" element={<Basket />} />
-          <Route path="/about" element={<AboutUs />} />
-        </Routes>
-        <Toaster position="top-center" richColors />
-      </BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="UI-THEME">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/catalog/:id" element={<ProductDetail />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/basket" element={<Basket />} />
+            <Route path="/about" element={<AboutUs />} />
+          </Routes>
+          <Toaster position="top-center" richColors closeButton />
+        </BrowserRouter>
+      </ThemeProvider>
     </AuthProvider>
   );
 }
