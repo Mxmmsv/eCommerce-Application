7d1e8fbf4ba3f3ec5fd62607d984f14644981178
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-18 18:38:00 +0300

    style: change the size of the toggle

--- src/feature/header/mode-toggle.tsx
+++ src/feature/header/mode-toggle.tsx
@@ -12,8 +12,8 @@ export function ModeToggle() {
 
   return (
     <Toggle onClick={toggleTheme}>
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 [&_svg:not([class*='size-'])]:size-6" />
+      <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 [&_svg:not([class*='size-'])]:size-6" />
     </Toggle>
   );
 }
