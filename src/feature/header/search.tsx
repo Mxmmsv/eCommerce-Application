4f076211d58c97cd7b5b1f186bc8c2ccbffd768f
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-18 14:39:58 +0300

    refactor: change ui component for search line

--- src/feature/header/search.tsx
+++ src/feature/header/search.tsx
@@ -1,16 +1,9 @@
-import { Search } from 'lucide-react';
-
-import { Input } from '@/components/ui/input';
+import { Command, CommandInput } from '@/components/ui/command';
 
 export default function SearchBar() {
   return (
-    <div className="flex max-w-3xs min-w-16 items-center gap-2 border-b bg-white p-2">
-      <Search size={32} />
-      <Input
-        type="text"
-        placeholder="Search"
-        className="font-rubik border-white text-base font-normal tracking-wider shadow-none sm:text-lg md:text-xl"
-      />
-    </div>
+    <Command className="flex min-w-40 rounded-lg border shadow-md">
+      <CommandInput placeholder="search..." />
+    </Command>
   );
 }
