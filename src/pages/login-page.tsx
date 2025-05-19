c806382cb0b86d0e96b1cad83bc29cbbeeb3a11d
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-20 00:19:37 +0300

    refactor: remove scrollbar on login page

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted flex min-h-[calc(100svh-20svh)] flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <LoginForm />
       </div>
