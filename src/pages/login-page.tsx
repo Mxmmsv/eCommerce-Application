adcc71a0fe8f400b402f1d1858f12adf11b555ec
Mxmmsv <61119540+Mxmmsv@users.noreply.github.com>
2025-05-20 01:30:30 +0300

    refactor: Remove logo + title from login page
    * refactor: remove logo + title from login page
    
    * refactor: remove unused file
    
    * refactor: remove scrollbar on login page

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Link, Navigate } from 'react-router';
+import { Navigate } from 'react-router';
 
 import AuthContext from '@/feature/auth/login/auth-provider';
 import { LoginForm } from '@/feature/auth/login/login-form';
@@ -12,14 +12,8 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted flex min-h-[calc(100svh-20svh)] flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link to="/" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-10 w-10 items-center justify-center rounded-md bg-white">
-            <img src="logo.svg" alt="logo" />
-          </div>
-          Poster Shop
-        </Link>
         <LoginForm />
       </div>
     </div>
