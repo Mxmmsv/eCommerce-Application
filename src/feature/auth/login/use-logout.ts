6f1d2ca6ae6b0705b4db36ec532f509e6ba08471
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-20 02:22:41 +0300

    refactor: remove dublicate code, set right name for fn

--- src/feature/auth/login/use-logout.ts
+++ src/feature/auth/login/use-logout.ts
@@ -4,13 +4,12 @@ import { useNavigate } from 'react-router';
 import AuthContext from '@/feature/auth/login/auth-provider';
 
 export function useLogout() {
-  const { setAuth } = useContext(AuthContext);
+  const { setIsAuthorized } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem('ACCESS_TOKEN_KEY');
-    localStorage.setItem('AUTH_STATUS_KEY', 'false');
-    setAuth(false);
+    setIsAuthorized(false);
     void navigate('/login');
   };
 
