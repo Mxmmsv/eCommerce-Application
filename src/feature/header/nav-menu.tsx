c17e305416f241feb3c696c9f19500a93793a6ac
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-23 13:26:41 +0300

    fix: (ECA-85) fix hovers in navigation menu

--- src/feature/header/nav-menu.tsx
+++ src/feature/header/nav-menu.tsx
@@ -10,18 +10,22 @@ export function CustomNavigationMenu() {
   return (
     <NavigationMenu className="flex items-center justify-between gap-5 text-xl">
       <NavigationMenuList className="flex items-center justify-between">
-        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer items-center justify-between rounded-sm px-6 py-4 text-xl font-medium">
-          <NavLink to="/" end>
+        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer items-center justify-between rounded-sm text-xl font-medium">
+          <NavLink to="/" end className="px-6 py-4">
             Home
           </NavLink>
         </NavigationMenuItem>
 
-        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer items-center justify-between rounded-sm px-6 py-4 text-xl font-medium">
-          <NavLink to="/catalog">Catalog</NavLink>
+        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer items-center justify-between rounded-sm text-xl font-medium">
+          <NavLink to="/catalog" className="px-6 py-4">
+            Catalog
+          </NavLink>
         </NavigationMenuItem>
 
-        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer justify-between self-center rounded-sm px-6 py-4 text-xl font-medium">
-          <NavLink to="/about_us">About us</NavLink>
+        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer justify-between self-center rounded-sm text-xl font-medium">
+          <NavLink to="/about_us" className="px-6 py-4">
+            About us
+          </NavLink>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
