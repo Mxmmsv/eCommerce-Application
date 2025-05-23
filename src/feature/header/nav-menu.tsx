5231eb263b53f9cc02f5b9a9560e360677439e35
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-23 15:13:32 +0300

    style: add stiles for active link in navigation menu

--- src/feature/header/nav-menu.tsx
+++ src/feature/header/nav-menu.tsx
@@ -10,20 +10,36 @@ export function CustomNavigationMenu() {
   return (
     <NavigationMenu className="flex items-center justify-between gap-5 text-xl">
       <NavigationMenuList className="flex items-center justify-between">
-        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer items-center justify-between rounded-sm text-xl font-medium">
-          <NavLink to="/" end className="px-6 py-4">
+        <NavigationMenuItem className="hover:text-chart-3 flex h-auto items-center justify-between rounded-sm text-xl font-medium">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              `before:text-chart-3 relative px-6 py-4 transition-all duration-300 ease-in-out before:absolute before:right-0 before:bottom-2 before:left-0 before:mx-auto before:h-[2px] before:w-0 before:bg-current before:transition-all before:duration-300 before:content-[''] hover:before:w-[calc(100%-3rem)] ${isActive ? 'text-chart-3 cursor-default before:w-[calc(100%-3rem)]' : ''}`
+            }
+          >
             Home
           </NavLink>
         </NavigationMenuItem>
 
-        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer items-center justify-between rounded-sm text-xl font-medium">
-          <NavLink to="/catalog" className="px-6 py-4">
+        <NavigationMenuItem className="hover:text-chart-3 flex h-auto items-center justify-between rounded-sm text-xl font-medium">
+          <NavLink
+            to="/catalog"
+            className={({ isActive }) =>
+              `before:text-chart-3 relative px-6 py-4 transition-all duration-300 ease-in-out before:absolute before:right-0 before:bottom-2 before:left-0 before:mx-auto before:h-[2px] before:w-0 before:bg-current before:transition-all before:duration-300 before:content-[''] hover:before:w-[calc(100%-3rem)] ${isActive ? 'text-chart-3 cursor-default before:w-[calc(100%-3rem)]' : ''}`
+            }
+          >
             Catalog
           </NavLink>
         </NavigationMenuItem>
 
-        <NavigationMenuItem className="hover:bg-secondary flex h-auto cursor-pointer justify-between self-center rounded-sm text-xl font-medium">
-          <NavLink to="/about_us" className="px-6 py-4">
+        <NavigationMenuItem className="hover:text-chart-3 flex h-auto justify-between self-center rounded-sm text-xl font-medium">
+          <NavLink
+            to="/about_us"
+            className={({ isActive }) =>
+              `before:text-chart-3 relative px-6 py-4 transition-all duration-300 ease-in-out before:absolute before:right-0 before:bottom-2 before:left-0 before:mx-auto before:h-[2px] before:w-0 before:bg-current before:transition-all before:duration-300 before:content-[''] hover:before:w-[calc(100%-3rem)] ${isActive ? 'text-chart-3 cursor-default before:w-[calc(100%-3rem)]' : ''}`
+            }
+          >
             About us
           </NavLink>
         </NavigationMenuItem>
