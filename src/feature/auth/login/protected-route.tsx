7c453a6b590f5a28e1d003abd82210839850f22b
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-24 21:55:47 +0300

    refactor: add import for ReactNode, improve code

--- src/feature/auth/login/protected-route.tsx
+++ src/feature/auth/login/protected-route.tsx
@@ -1,23 +1,21 @@
-import { useContext, useEffect, useRef } from 'react';
-import { Navigate } from 'react-router';
+import { useContext, useEffect, useRef, type ReactNode } from 'react';
+import { useNavigate } from 'react-router';
 import { toast } from 'sonner';
 
 import AuthContext from './auth-provider';
 
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { IS_AUTHORIZED } = useContext(AuthContext);
   const hasShownToast = useRef(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!IS_AUTHORIZED && !hasShownToast.current) {
       toast.warning('Oops! You need to log in first.');
       hasShownToast.current = true;
+      void navigate('/login');
     }
-  }, [IS_AUTHORIZED]);
-
-  if (!IS_AUTHORIZED) {
-    return <Navigate to="/login" replace />;
-  }
+  }, [IS_AUTHORIZED, navigate]);
 
   return children;
 };
