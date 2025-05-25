8b34b3622f001b4770f8b7b51b602c72b1eafd8d
ek-ole <ek_34@mail.ru>
2025-05-25 13:50:42 +0300

    refactor: separate the product-component

--- src/feature/catalog/product-component.tsx
+++ src/feature/catalog/product-component.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Link } from 'react-router';
 
 import type { Poster } from './types';
@@ -25,6 +26,40 @@ const ProductComponent = ({ poster }: { poster: Poster }) => {
           </div>
         </div>
       </Link>
+=======
+import type { Product } from '@/feature/catalog/types';
+
+type ProductComponentProps = {
+  product: Product;
+};
+
+const ProductComponent = ({ product }: ProductComponentProps) => {
+  return (
+    <div
+      key={product.id}
+      className="bg-background border-border flex flex-col overflow-clip border"
+    >
+      <div className="px-6 py-8 md:px-6 md:py-10 lg:px-6 lg:py-8">
+        <div className="mb-6 md:mb-6 lg:mb-6">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="aspect-3/4 w-full object-cover"
+            onError={(e) => {
+              (e.target as HTMLImageElement).src = '/placeholder-product.webp';
+            }}
+          />
+        </div>
+        <div>
+          <h3 className="mb-3 text-lg font-semibold md:mb-3 md:text-xl lg:mb-3 lg:text-xl">
+            {product.name || 'Not available'}
+          </h3>
+          <p className="text-muted-foreground lg:text-lg">
+            {product.description || 'No description available'}
+          </p>
+        </div>
+      </div>
+>>>>>>> cd0ce35 (refactor: separate the product-component)
     </div>
   );
 };
--- src/feature/catalog/product-list.tsx
+++ src/feature/catalog/product-list.tsx
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 import { ProductComponent } from './product-component';
 import type { Poster } from './types';
 
 type ProductListProps = {
   products: Poster[];
+=======
+import type { Product } from '@/feature/catalog/types';
+
+import { ProductComponent } from './product-component';
+
+type ProductListProps = {
+  products: Product[];
+>>>>>>> cd0ce35 (refactor: separate the product-component)
 };
 
 const ProductList = ({ products }: ProductListProps) => {
@@ -11,11 +20,19 @@ const ProductList = ({ products }: ProductListProps) => {
   }
 
   return (
+<<<<<<< HEAD
     <section className="py-6">
       <div className="container flex flex-col gap-16 lg:px-16">
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 lg:gap-8">
           {products.map((poster) => (
             <ProductComponent key={poster.id} poster={poster} />
+=======
+    <section className="py-32">
+      <div className="container flex flex-col gap-16 lg:px-16">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4 lg:gap-8">
+          {products.map((product) => (
+            <ProductComponent key={product.id} product={product} />
+>>>>>>> cd0ce35 (refactor: separate the product-component)
           ))}
         </div>
       </div>
