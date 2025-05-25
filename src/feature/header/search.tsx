aecb37a1b160f4a66ac7f6707aa2c7be7b601cb4
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-25 19:52:36 +0300

    style: style the search module for screen more thаn 1025px

--- src/feature/header/search.tsx
+++ src/feature/header/search.tsx
@@ -2,7 +2,7 @@ import { Command, CommandInput } from '@/components/ui/command';
 
 export default function SearchBar() {
   return (
-    <Command className="flex rounded-lg border shadow-md">
+    <Command className="rounded-lg border shadow-md lg:flex">
       <CommandInput className="outline-none" placeholder="search..." />
     </Command>
   );
