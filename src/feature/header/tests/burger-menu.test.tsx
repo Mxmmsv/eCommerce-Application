0f0a9137d286a228ca5365a7590ee4100af2476f
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-05-25 14:32:48 +0300

    test: refactor test for burger menu

--- src/feature/header/tests/burger-menu.test.tsx
+++ src/feature/header/tests/burger-menu.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router';
 import { describe, it, expect } from 'vitest';
@@ -6,47 +6,45 @@ import { describe, it, expect } from 'vitest';
 import BurgerMenu from '../burger-menu';
 
 describe('BurgerMenu', () => {
-  it('opens when clicking menu-open-button and closes when clicking on X-button', async () => {
+  it('opens when click on menu-open-button and closes when click on X-button', async () => {
+    const user = userEvent.setup();
+
     render(
       <MemoryRouter>
         <BurgerMenu />
       </MemoryRouter>,
     );
-    const user = userEvent.setup();
 
-    const openMenuButton = screen.getByRole('button', { name: 'open menu' });
+    const openMenuButton = screen.getByRole('button', { name: /open menu/i });
     await user.click(openMenuButton);
 
-    const burgerMenu = await screen.findByRole('dialog', { name: /burger menu/i });
+    const burgerMenu = screen.getByRole('dialog', { name: /burger menu/i });
     expect(burgerMenu).toHaveAttribute('data-state', 'open');
 
-    const closeMenuButton = screen.getByRole('button', { name: 'close menu' });
+    const closeMenuButton = screen.getByRole('button', { name: /close menu/i });
     await user.click(closeMenuButton);
 
-    await waitFor(() => {
-      expect(burgerMenu).toHaveAttribute('data-state', 'closed');
-    });
+    expect(burgerMenu).toHaveAttribute('data-state', 'closed');
   });
 
   it('closes the burger menu after clicking a link', async () => {
+    const user = userEvent.setup();
+
     render(
       <MemoryRouter>
         <BurgerMenu />
       </MemoryRouter>,
     );
-    const user = userEvent.setup();
 
-    const openMenuButton = screen.findByRole('button', { name: /open menu/i });
-    await user.click(await openMenuButton);
+    const openMenuButton = screen.getByRole('button', { name: /open menu/i });
+    await user.click(openMenuButton);
 
-    const burgerMenu = await screen.findByRole('dialog', { name: /burger menu/i });
+    const burgerMenu = screen.getByRole('dialog', { name: /burger menu/i });
     expect(burgerMenu).toBeInTheDocument();
 
     const link = screen.getByRole('link', { name: /wishlist/i });
     await user.click(link);
 
-    await waitFor(() => {
-      expect(screen.queryByRole('dialog', { name: /burger menu/i })).not.toBeInTheDocument();
-    });
+    expect(screen.queryByRole('dialog', { name: /burger menu/i })).not.toBeInTheDocument();
   });
 });
