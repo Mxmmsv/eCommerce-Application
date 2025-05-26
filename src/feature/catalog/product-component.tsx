128286fb2b52124c65877d22c9d8e74eed9eca8c
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-26 19:18:28 +0300

    feat: add link to product overview

--- src/feature/catalog/product-component.tsx
+++ src/feature/catalog/product-component.tsx
@@ -1,4 +1,5 @@
 import type { Product } from '@commercetools/platform-sdk';
+import { Link } from 'react-router';
 
 type ProductComponentProps = {
   product: Product;
@@ -14,26 +15,28 @@ const ProductComponent = ({ product }: ProductComponentProps) => {
       key={product.id}
       className="bg-background border-border flex flex-col overflow-clip border"
     >
-      <div className="px-6 py-8 md:px-6 md:py-10 lg:px-6 lg:py-8">
-        <div className="mb-6 md:mb-6 lg:mb-6">
-          <img
-            src={image}
-            alt={name}
-            className="aspect-3/4 w-full object-cover"
-            onError={(e) => {
-              (e.target as HTMLImageElement).src = '/placeholder-product.webp';
-            }}
-          />
+      <Link to={`/catalog/${product.id}`}>
+        <div className="px-6 py-8 md:px-6 md:py-10 lg:px-6 lg:py-8">
+          <div className="mb-6 md:mb-6 lg:mb-6">
+            <img
+              src={image}
+              alt={name}
+              className="aspect-3/4 w-full object-cover"
+              onError={(e) => {
+                (e.target as HTMLImageElement).src = '/placeholder-product.webp';
+              }}
+            />
+          </div>
+          <div>
+            <h3 className="mb-3 text-lg font-semibold md:mb-3 md:text-xl lg:mb-3 lg:text-xl">
+              {name || 'Not available'}
+            </h3>
+            <p className="text-muted-foreground lg:text-lg">
+              {description || 'No description available'}
+            </p>
+          </div>
         </div>
-        <div>
-          <h3 className="mb-3 text-lg font-semibold md:mb-3 md:text-xl lg:mb-3 lg:text-xl">
-            {name || 'Not available'}
-          </h3>
-          <p className="text-muted-foreground lg:text-lg">
-            {description || 'No description available'}
-          </p>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
