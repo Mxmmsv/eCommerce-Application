08452db5c6641161488d4c130b6deacf69d02483
ek-ole <117985918+ek-ole@users.noreply.github.com>
2025-05-26 16:35:14 +0300

    feat: Display catalog
    * feat: add product display to catalog page
    
    * refactor: code simplification and renaming
    
    * feat: add placeholder if there is no image
    
    * feat: add spiner
    
    * fix: delete unnecessary files
    
    * refactor: change palceholder size
    
    * refactor: separate the product-component
    
    * refactor: add type from sdk, add swr

--- src/pages/catalog-page.tsx
+++ src/pages/catalog-page.tsx
@@ -1,7 +1,45 @@
+import type { Product } from '@commercetools/platform-sdk';
+import useSWR from 'swr';
+
+import { Spinner } from '@/components/ui/spiner';
+import { fetchProducts } from '@/feature/catalog/api/fetch-products';
+import { ProductList } from '@/feature/catalog/product-list';
+
+const fetcher = async () => {
+  const products = await fetchProducts();
+  return products;
+};
+
 export default function CatalogPage() {
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useSWR<Product[], Error>('commercetools/products', fetcher);
+
+  if (isLoading) {
+    return (
+      <div className="column flex min-h-svh items-center justify-center">
+        <Spinner size="medium" className="text-primary">
+          <span className="text-center">Loading products...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex min-h-svh items-center justify-center">
+        <div className="text-center text-red-500">{error.message || 'Failed to load products'}</div>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-muted flex min-h-svh items-center justify-center text-lg">
-      <h1>Catalog Product Page</h1>
+      <div className="container py-8">
+        <ProductList products={products || []} />
+      </div>
     </div>
   );
 }
