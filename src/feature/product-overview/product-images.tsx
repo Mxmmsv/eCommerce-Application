bc9274d196a3865094c3229bb1d72f93d810a0c9
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-28 13:42:30 +0300

    fix: update slider button position

--- src/feature/product-overview/product-images.tsx
+++ src/feature/product-overview/product-images.tsx
@@ -11,10 +11,12 @@ import type { ProductData } from './types';
 export function ProductImages({ image, alt }: Pick<ProductData, 'image' | 'alt'>) {
   if (image.length === 1) {
     return (
-      <div className="flex aspect-square items-center justify-center p-6">
+      <div className="flex aspect-square items-center justify-center">
         <img
           src={image[0].url}
           alt={alt}
+          loading="lazy"
+          decoding="sync"
           className="h-full w-full rounded-2xl bg-white object-cover"
         />
       </div>
@@ -25,17 +27,22 @@ export function ProductImages({ image, alt }: Pick<ProductData, 'image' | 'alt'>
     <Carousel className="h-full w-full">
       <CarouselContent>
         {image.map((image, index) => (
-          <CarouselItem key={index} className="flex aspect-square items-center justify-center p-6">
+          <CarouselItem key={index} className="flex aspect-square items-center justify-center">
             <img
               src={image.url}
               alt={alt}
+              loading="lazy"
+              decoding="async"
               className="h-full w-full rounded-2xl bg-white object-cover"
             />
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <CarouselPrevious
+        className="top-1/2 left-2 -translate-y-1/2 transform"
+        variant={'secondary'}
+      />
+      <CarouselNext className="top-1/2 right-2 -translate-y-1/2 transform" variant={'secondary'} />
     </Carousel>
   );
 }
