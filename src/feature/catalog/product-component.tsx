de25da68e63580870245545801c167cb55fd41a0
ek-ole <ek_34@mail.ru>
2025-05-28 14:48:23 +0300

    refactor: change size of image

--- src/feature/catalog/product-component.tsx
+++ src/feature/catalog/product-component.tsx
@@ -4,21 +4,21 @@ const ProductComponent = ({ poster }: { poster: Poster }) => {
   return (
     <div key={poster.id} className="bg-background border-border flex flex-col overflow-clip border">
       <div className="px-6 py-8 md:px-6 md:py-10 lg:px-6 lg:py-8">
-        <div className="mb-6 md:mb-6 lg:mb-6">
+        <div className="relative aspect-[2/3] w-full overflow-hidden">
           <img
             src={poster.image}
             alt={poster.name}
-            className="aspect-3/4 w-full object-cover"
+            className="h-full w-full object-contain"
             onError={(e) => {
               (e.target as HTMLImageElement).src = '/placeholder-product.webp';
             }}
           />
         </div>
         <div>
-          <h3 className="mb-3 text-lg font-semibold md:mb-3 md:text-xl lg:mb-3 lg:text-xl">
+          <h3 className="mt-3 text-lg font-semibold md:mb-3 md:text-xl lg:mb-3 lg:text-base">
             {poster.name}
           </h3>
-          <p className="text-muted-foreground text-sm">{poster.description}</p>
+          <p className="text-muted-foreground line-clamp-2 text-sm">{poster.description}</p>
         </div>
       </div>
     </div>
