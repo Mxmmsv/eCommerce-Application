fb075629bb670713348f56d634c587868c581301
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-29 13:37:47 +0300

    feat: (ECA-70) implement modal window

--- src/feature/product-overview/product-images.tsx
+++ src/feature/product-overview/product-images.tsx
@@ -5,6 +5,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 
 import type { ProductData } from './types';
 
@@ -28,21 +29,43 @@ export function ProductImages({ image, alt }: Pick<ProductData, 'image' | 'alt'>
       <CarouselContent>
         {image.map((image, index) => (
           <CarouselItem key={index} className="flex aspect-square items-center justify-center">
-            <img
-              src={image.url}
-              alt={alt}
-              loading="lazy"
-              decoding="async"
-              className="bg-muted-foreground h-full w-full rounded-2xl object-contain"
-            />
+            <Dialog>
+              <DialogTrigger asChild>
+                <button
+                  type="button"
+                  className="h-full w-full cursor-zoom-in rounded-2xl bg-transparent p-0"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <img
+                    src={image.url}
+                    alt={alt}
+                    loading="lazy"
+                    decoding="async"
+                    className="bg-muted-foreground h-full w-full object-contain"
+                  />
+                </button>
+              </DialogTrigger>
+
+              <DialogContent className="h-full w-full">
+                <img
+                  src={image.url}
+                  alt={alt}
+                  className="h-full w-full rounded-xl object-contain"
+                />
+              </DialogContent>
+            </Dialog>
           </CarouselItem>
         ))}
       </CarouselContent>
+
       <CarouselPrevious
-        className="top-1/2 left-2 -translate-y-1/2 transform"
-        variant={'secondary'}
+        className="top-1/2 left-2 z-10 -translate-y-1/2 transform"
+        variant="secondary"
       />
-      <CarouselNext className="top-1/2 right-2 -translate-y-1/2 transform" variant={'secondary'} />
+      <CarouselNext
+        className="top-1/2 right-2 z-10 -translate-y-1/2 transform"
+        variant="secondary"
+      />
     </Carousel>
   );
 }
