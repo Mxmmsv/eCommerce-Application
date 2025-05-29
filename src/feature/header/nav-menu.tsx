ba94309038fcae93c4e123e47dc3a060cfe8f3c2
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-29 19:36:30 +0300

    feat: (ECA-90) add cursor pointer for path `/catalog/category/***`

--- src/feature/header/nav-menu.tsx
+++ src/feature/header/nav-menu.tsx
@@ -10,7 +10,8 @@ import { cn } from '@/lib/utils';
 export function CustomNavigationMenu() {
   const location = useLocation();
   const path = location.pathname;
-  const isCatalogActive = path === '/catalog' || /^\/catalog\/category\/[^/]+$/.test(path);
+  const isCatalogActive = path === '/catalog';
+  const isCategoryActive = /^\/catalog\/category\/[^/]+$/.test(path);
   return (
     <NavigationMenu className="items-center justify-between gap-5 text-xl max-lg:hidden lg:flex">
       <NavigationMenuList className="flex items-center justify-between">
@@ -32,6 +33,7 @@ export function CustomNavigationMenu() {
             className={cn(
               "before:text-chart-3 relative px-6 py-4 transition-all duration-300 ease-in-out before:absolute before:right-0 before:bottom-2 before:left-0 before:mx-auto before:h-[2px] before:w-0 before:bg-current before:transition-all before:duration-300 before:content-[''] hover:before:w-[calc(100%-3rem)]",
               isCatalogActive && 'text-chart-3 cursor-default before:w-[calc(100%-3rem)]',
+              isCategoryActive && 'text-chart-3 before:w-[calc(100%-3rem)]',
             )}
           >
             Catalog
