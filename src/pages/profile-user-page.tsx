482e32468ef3c199b3784600fe982df1767f8178
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-30 22:03:12 +0300

    feat: (ECA-88) add dynamic title change

--- src/pages/profile-user-page.tsx
+++ src/pages/profile-user-page.tsx
@@ -6,23 +6,26 @@ export default function UserProfilePage() {
   const customer = useCustomerStore((state) => state.customer);
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center text-lg">
-      <h1 className="text-2xl font-bold">User Profile Page</h1>
-      {customer !== null && (
-        <span>
-          Hello, {customer?.firstName} {customer?.lastName}
-        </span>
-      )}
-      <div className="mt-4 flex flex-col items-center justify-center">
-        <Link to="/login" className="text-blue-500 hover:underline">
-          Login
-        </Link>
-        <Link to="/registration" className="text-blue-500 hover:underline">
-          Registration
-        </Link>
-      </div>
+    <>
+      <title>{'Profile :: Poster store'}</title>
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center text-lg">
+        <h1 className="text-2xl font-bold">User Profile Page</h1>
+        {customer !== null && (
+          <span>
+            Hello, {customer?.firstName} {customer?.lastName}
+          </span>
+        )}
+        <div className="mt-4 flex flex-col items-center justify-center">
+          <Link to="/login" className="text-blue-500 hover:underline">
+            Login
+          </Link>
+          <Link to="/registration" className="text-blue-500 hover:underline">
+            Registration
+          </Link>
+        </div>
 
-      <Outlet />
-    </div>
+        <Outlet />
+      </div>
+    </>
   );
 }
