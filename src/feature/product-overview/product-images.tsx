695cca35f644cf6a5db9e95dba50756f7368594f
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-05-30 17:53:59 +0300

    refactor: add grid layout for slider image

--- src/feature/product-overview/product-images.tsx
+++ src/feature/product-overview/product-images.tsx
@@ -47,50 +47,46 @@ export function ProductImages({
   }
 
   return (
-    <Carousel>
-      <CarouselContent>
-        {imageArr.map((image, index) => (
-          <CarouselItem key={index} className="flex aspect-square items-center justify-center">
-            <Dialog>
-              <DialogTrigger asChild>
-                <img
-                  src={image.url}
-                  alt={alt}
-                  loading="lazy"
-                  decoding="async"
-                  className="bg-muted-foreground h-full w-full cursor-zoom-in rounded-2xl object-contain"
-                />
-              </DialogTrigger>
+    <div className="grid grid-cols-5 grid-rows-4">
+      <Carousel className="col-span-3 col-start-2 row-span-4 row-start-1">
+        <CarouselContent>
+          {imageArr.map((image, index) => (
+            <CarouselItem key={index} className="flex aspect-square items-center justify-center">
+              <Dialog>
+                <DialogTrigger asChild>
+                  <img
+                    src={image.url}
+                    alt={alt}
+                    loading="lazy"
+                    decoding="async"
+                    className="bg-muted-foreground h-full w-full cursor-zoom-in rounded-2xl object-contain"
+                  />
+                </DialogTrigger>
 
-              <DialogContent>
-                <DialogTitle hidden>{alt}</DialogTitle>
-                <DialogDescription hidden>{description}</DialogDescription>
+                <DialogContent>
+                  <DialogTitle hidden>{alt}</DialogTitle>
+                  <DialogDescription hidden>{description}</DialogDescription>
 
-                <Carousel>
-                  <CarouselContent>
-                    {imageArr.map((image, index) => (
-                      <CarouselItem key={index} className="flex items-center justify-center">
-                        <img src={image.url} alt={alt} loading="lazy" decoding="async" />
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious />
-                  <CarouselNext />
-                </Carousel>
-              </DialogContent>
-            </Dialog>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
+                  <Carousel>
+                    <CarouselContent>
+                      {imageArr.map((image, index) => (
+                        <CarouselItem key={index} className="flex items-center justify-center">
+                          <img src={image.url} alt={alt} loading="lazy" decoding="async" />
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                  </Carousel>
+                </DialogContent>
+              </Dialog>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
 
-      <CarouselPrevious
-        className="top-1/2 left-2 z-10 -translate-y-1/2 transform"
-        variant="secondary"
-      />
-      <CarouselNext
-        className="top-1/2 right-2 z-10 -translate-y-1/2 transform"
-        variant="secondary"
-      />
-    </Carousel>
+        <CarouselPrevious className="col-start-1 row-span-4 row-start-1" variant="secondary" />
+        <CarouselNext className="col-start-5 row-span-4" variant="secondary" />
+      </Carousel>
+    </div>
   );
 }
