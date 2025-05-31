7c1b056086ce49aea452d97e80435a47931a905f
ek-ole <117985918+ek-ole@users.noreply.github.com>
2025-05-31 23:55:28 +0300

    feat: Implement Catalog price
    * feat: add product display to catalog page
    
    * refactor: code simplification and renaming
    
    * feat: add placeholder if there is no image
    
    * feat: add spiner
    
    * feat: add product display to catalog page
    
    * refactor: code simplification and renaming
    
    * feat: add placeholder if there is no image
    
    * feat: add spiner
    
    * fix: delete unnecessary files
    
    * refactor: change palceholder size
    
    * refactor: separate the product-component
    
    * refactor: add type from sdk, add swr
    
    * feat: add categories of product and filter by it
    
    * refactor: apply map to product
    
    * feat: add categories tree
    
    * feat: add library for category tree
    
    * feat: implement category nesting
    
    * refactor: change the appearance of the list of nested categories
    
    * refactor: change category file structure
    
    * refactor: change category file structure
    
    * refactor: rafactor categories
    
    * refactor: refactor breadcrumbs
    
    * refactor: refactor categoty-navigation
    
    * refactor: refactor categoty-navigation
    
    * refactor: add styles for dropdown menu
    
    * refactor: change paddings for dropdown menu
    
    * refactor: correct type
    
    * refactor: fix dropdown menu
    
    * refactor: correct type more
    
    * refactor: change size of image
    
    * feat: add price
    
    * feat: add link to product page
    
    * fix: fix history when clicking on all categories
    
    * fix: add an hover to the catalog from the product page
    
    * feat: add product display to catalog page
    
    * refactor: code simplification and renaming
    
    * feat: add placeholder if there is no image
    
    * feat: add spiner
    
    * feat: add product display to catalog page
    
    * refactor: code simplification and renaming
    
    * feat: add placeholder if there is no image
    
    * feat: add spiner
    
    * fix: delete unnecessary files
    
    * refactor: change palceholder size
    
    * refactor: separate the product-component
    
    * refactor: add type from sdk, add swr
    
    * feat: add categories of product and filter by it
    
    * feat: add categories tree
    
    * feat: add library for category tree
    
    * feat: implement category nesting
    
    * refactor: change the appearance of the list of nested categories
    
    * refactor: change category file structure
    
    * refactor: change category file structure
    
    * refactor: rafactor categories
    
    * refactor: refactor breadcrumbs
    
    * refactor: refactor categoty-navigation
    
    * refactor: refactor categoty-navigation
    
    * refactor: correct type
    
    * refactor: change size of image
    
    * feat: add price
    
    * fix: fix history when clicking on all categories
    
    * fix: add an hover to the catalog from the product page
    
    * fix: merging
    
    * refactor: resolve conflict

--- src/feature/catalog/types.ts
+++ src/feature/catalog/types.ts
@@ -3,6 +3,9 @@ export type Poster = {
   name: string;
   description: string;
   image: string;
+  price: string;
+  currencyCode: string;
+  discount?: string;
 };
 
 export type PosterCategory = {
@@ -26,4 +29,9 @@ export type CategoryDropdownProps = {
   onSelect: (category: PosterCategory) => void;
   openedSubmenus: Record<string, boolean>;
   toggleSubmenu: (id: string) => void;
+};
+
+export type CategoryPathItem = {
+  id: string;
+  name: string;
 };
