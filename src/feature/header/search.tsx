00053b8778609a46ab8329c628cd2f6321131630
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-02 13:20:16 +0300

    refactor: remove not used logic

--- src/feature/header/search.tsx
+++ src/feature/header/search.tsx
@@ -2,21 +2,14 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import { Command, CommandInput } from '@/components/ui/command';
-import { useCategoryStore } from '@/service/store/use-category-store';
 
 export default function SearchBar() {
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState('');
-  const { currentPath } = useCategoryStore();
-  const lastCategoryId = currentPath[currentPath.length - 1]?.id;
 
   const handleSearch = (value: string) => {
     setSearchQuery(value);
-    const params = new URLSearchParams();
-    if (value) params.set('search', value);
-    if (lastCategoryId) params.set('category', lastCategoryId);
-
-    void navigate(`/catalog?${params}`);
+    void navigate(`/catalog?search=${value}`);
   };
 
   return (
