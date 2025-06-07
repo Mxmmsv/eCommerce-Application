be6204baac362ed99ce6aef88b1cb255a46a55f9
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-06-07 20:27:06 +0300

    feat: (ECA-94) add addToCart function with toast notification

---
+++ src/feature/catalog/adding-to-cart/cart-actions.ts
@@ -0,0 +1,56 @@
+import { toast } from 'sonner';
+
+import apiClientAnonymous from '@/feature/api/api-client-anonymous';
+import { createApiClientWithToken } from '@/feature/api/api-client-token-flow';
+import { useCartStore } from '@/feature/catalog/adding-to-cart/use-cart-store';
+
+export async function addToCart(productId: string, isAuthenticated: boolean, token: string | null) {
+  const { cartId, setCart } = useCartStore.getState();
+
+  const apiClient = isAuthenticated && token ? createApiClientWithToken(token) : apiClientAnonymous;
+
+  const resolvedApiClient = typeof apiClient === 'function' ? apiClient() : apiClient;
+
+  try {
+    let cart = null;
+
+    if (!cartId) {
+      const response = await resolvedApiClient
+        .carts()
+        .post({
+          body: {
+            currency: 'EUR',
+          },
+        })
+        .execute();
+      cart = response.body;
+      setCart(cart);
+    } else {
+      const response = await resolvedApiClient.carts().withId({ ID: cartId }).get().execute();
+      cart = response.body;
+    }
+
+    const updatedCartResponse = await resolvedApiClient
+      .carts()
+      .withId({ ID: cart.id })
+      .post({
+        body: {
+          version: cart.version,
+          actions: [
+            {
+              action: 'addLineItem',
+              productId,
+              quantity: 1,
+            },
+          ],
+        },
+      })
+      .execute();
+
+    setCart(updatedCartResponse.body);
+    toast.info('Product added to cart successfully!');
+    return updatedCartResponse.body;
+  } catch (error) {
+    console.error('Failed to add to cart', error);
+  }
+}
