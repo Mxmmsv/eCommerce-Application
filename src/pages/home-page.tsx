84a5b9e2e33eae7bf0fdd8eda03cf0d3b5acf4f0
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-12 19:29:07 +0300

    feat: (ECA-113) add banner and color for hero section

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -4,9 +4,7 @@ export default function HomePage() {
   return (
     <>
       <title>{'Home :: Poster store'}</title>
-      <div className="bg-muted flex min-h-svh items-center justify-center text-lg">
-        <HeroSection />
-      </div>
+      <HeroSection />
     </>
   );
 }
