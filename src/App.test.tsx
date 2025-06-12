72360444fb2ef182b6d4db557f39eb73a0f45247
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-12 17:58:21 +0300

    test: update app.tsx test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,7 +8,9 @@ describe('App', () => {
   it('renders content on the pages and navigates to the pages', async () => {
     render(<App />);
     const user = userEvent.setup();
-    expect(screen.getByRole('heading', { name: /home page/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Posters Are More Than Just Decor/i }),
+    ).toBeInTheDocument();
 
     await user.click(screen.getByRole('link', { name: /about us/i }));
     expect(screen.getByRole('heading', { name: /about us page/i })).toBeInTheDocument();
