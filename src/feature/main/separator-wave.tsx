5995ebfca2744728055dca7da0364cad5af297fe
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-13 17:28:29 +0300

    feat: add separator

---
+++ src/feature/main/separator-wave.tsx
@@ -0,0 +1,7 @@
+function SeparatorWave() {
+  return (
+    <div className="mt-12 h-3 w-full overflow-hidden bg-[url('/separator-wave.svg')] bg-[length:40px_10px] bg-center bg-repeat-x opacity-100" />
+  );
+}
+
+export default SeparatorWave;
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,12 +1,16 @@
 import EvolutionSection from '@/feature/main/evolution-section';
 import HeroSection from '@/feature/main/hero-section';
+import RemainSection from '@/feature/main/remain-section';
+import SeparatorWave from '@/feature/main/separator-wave';
 
 export default function HomePage() {
   return (
     <>
       <title>{'Home :: Poster store'}</title>
       <HeroSection />
+      <SeparatorWave />
       <EvolutionSection />
+      <RemainSection />
     </>
   );
 }
