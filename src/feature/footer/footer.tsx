acc8e3f4b5053a630f1bddbc332221e21f70c4fd
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-14 13:47:55 +0300

    refactor: change after review

--- src/feature/footer/footer.tsx
+++ src/feature/footer/footer.tsx
@@ -3,55 +3,55 @@ import { Link } from 'react-router';
 
 function Footer() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <div className="bg-muted grow" />
-      <footer>
-        <div className="mx-auto max-w-screen-xl">
-          <div className="flex flex-col-reverse items-center justify-between gap-x-2 gap-y-5 px-6 py-8 sm:flex-row xl:px-0">
-            <span className="text-muted-foreground">
+    <footer>
+      <div className="mx-auto max-w-screen-xl">
+        <div className="flex flex-col-reverse items-center justify-between gap-x-2 gap-y-5 px-6 py-8 sm:flex-row xl:px-0">
+          <div className="flex flex-col items-center md:items-start">
+            <p className="text-muted-foreground">
               &copy; {new Date().getFullYear()}{' '}
               <Link to="/" className="hover:text-chart-3 transition-colors duration-300">
                 Poster Store
               </Link>
-              . All rights reserved.
-            </span>
+              .
+            </p>
+            <p className="text-muted-foreground">All rights reserved.</p>
+          </div>
 
-            <div className="flex flex-col items-center justify-center">
-              <span className="text-muted-foreground">Rolling Scopes School</span>
-              <ul className="text-muted-foreground flex items-center gap-5">
-                <li>
-                  <Link
-                    to="https://rs.school"
-                    target="_blank"
-                    className="hover:text-chart-3 p-3 transition-colors duration-300 lg:p-2"
-                  >
-                    <DribbbleIcon className="h-5 w-5" />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="https://x.com/rollingscopes"
-                    target="_blank"
-                    className="hover:text-chart-3 p-3 transition-colors duration-300 lg:p-2"
-                  >
-                    <TwitterIcon className="h-5 w-5" />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="https://github.com/rolling-scopes-school"
-                    target="_blank"
-                    className="hover:text-chart-3 p-3 transition-colors duration-300 lg:p-2"
-                  >
-                    <GithubIcon className="h-5 w-5" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
+          <div className="flex flex-col items-center justify-center md:items-end">
+            <span className="text-muted-foreground">Rolling Scopes School</span>
+            <ul className="text-muted-foreground flex gap-5">
+              <li>
+                <Link
+                  to="https://rs.school"
+                  target="_blank"
+                  className="hover:text-chart-3 p-2 transition-colors duration-300 lg:p-1"
+                >
+                  <DribbbleIcon className="h-5 w-5" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://x.com/rollingscopes"
+                  target="_blank"
+                  className="hover:text-chart-3 p-2 transition-colors duration-300 lg:p-1"
+                >
+                  <TwitterIcon className="h-5 w-5" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://github.com/rolling-scopes-school"
+                  target="_blank"
+                  className="hover:text-chart-3 p-2 transition-colors duration-300 lg:p-1"
+                >
+                  <GithubIcon className="h-5 w-5" />
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 }
 
