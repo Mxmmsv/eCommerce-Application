ce9628c153ef302a1f1ff84ff0ea2334eeccf5da
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-14 18:34:53 +0300

    feat: add style for p

--- src/feature/main/remain-section.tsx
+++ src/feature/main/remain-section.tsx
@@ -8,28 +8,28 @@ function RemainSection() {
         <div className="grid grid-cols-1 gap-5 text-center md:grid-cols-2 lg:grid-cols-4 lg:text-balance">
           <div>
             <h3 className="text-xl font-semibold">Affordable Art Revolution</h3>
-            <p>
+            <p className="mt-6 max-w-[60ch] text-lg">
               While original masterpieces cost millions, posters bring iconic artworks within reach
               - letting you own a slice of cultural history without the auction-house price tag.
             </p>
           </div>
           <div>
             <h3 className="text-xl font-semibold">Personal Canvas</h3>
-            <p>
+            <p className="mt-6 max-w-[60ch] text-lg">
               Your walls become a moodboard of life - music legends, travel dreams, or abstract
               visions tell your story better than words ever could.
             </p>
           </div>
           <div>
             <h3 className="text-xl font-semibold">Design Chameleons</h3>
-            <p>
+            <p className="mt-6 max-w-[60ch] text-lg">
               They effortlessly adapt to your space - industrial loft one year, Scandinavian
               sanctuary the next. Mix, layer, and reimagine without commitment.
             </p>
           </div>
           <div>
             <h3 className="text-xl font-semibold">Instant Makeover Magic</h3>
-            <p>
+            <p className="mt-6 max-w-[60ch] text-lg">
               No contractors needed. Just swap a poster to completely transform your vibe - the
               ultimate low-commitment high-impact decor hack.
             </p>
