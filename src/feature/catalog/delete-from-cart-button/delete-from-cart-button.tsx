1ce9815d37575189b7994d81d5011722322d800b
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-06-15 17:56:37 +0300

    feat: (ECA-97) create remove from cart button

---
+++ src/feature/catalog/delete-from-cart-button/delete-from-cart-button.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+import { Button } from '@/components/ui/button';
+import { createApiClientWithToken } from '@/feature/api/api-client-token-flow';
+import { isHttpError } from '@/feature/api/errors';
+import { cn } from '@/lib/utils';
+
+import { useCartStore } from '../adding-to-cart/use-cart-store';
+
+type Props = {
+  productId: string;
+  variantId: number;
+};
+
+export const DeleteFromCartButton = ({ productId, variantId }: Props) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const { cart, setCart } = useCartStore();
+
+  const lineItem = cart?.lineItems.find(
+    (item) => item.productId === productId && item.variant.id === variantId,
+  );
+
+  const handleDelete = async () => {
+    if (!cart || !lineItem) return;
+
+    try {
+      setIsDeleting(true);
+      const apiClient = createApiClientWithToken();
+
+      const response = await apiClient
+        .me()
+        .carts()
+        .withId({ ID: cart.id })
+        .post({
+          body: {
+            version: cart.version,
+            actions: [
+              {
+                action: 'removeLineItem',
+                lineItemId: lineItem.id,
+              },
+            ],
+          },
+        })
+        .execute();
+
+      setCart(response.body);
+      toast.success('Product deleted from cart');
+    } catch (error) {
+      if (isHttpError(error)) {
+        toast.error(`Failed to delete product (error ${error.statusCode})`);
+      } else {
+        toast.error('Unknown error while deleting item from cart');
+      }
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
+  const buttonLabel = !lineItem
+    ? "Isn't in the cart"
+    : isDeleting
+      ? 'Deleting...'
+      : 'Delete from Cart';
+
+  const buttonClass = cn(
+    'flex-1 hover:bg-[#ff676e]',
+    !lineItem && 'cursor-not-allowed',
+    isDeleting && 'opacity-75',
+  );
+
+  return (
+    <Button
+      size="lg"
+      variant="destructive"
+      disabled={!lineItem || isDeleting}
+      onClick={() => {
+        void handleDelete();
+      }}
+      className={buttonClass}
+    >
+      {buttonLabel}
+    </Button>
+  );
+};
