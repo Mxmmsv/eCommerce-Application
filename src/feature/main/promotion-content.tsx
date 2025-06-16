6531eace9560bd1e7e9f603f24856020f1d2489b
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-06-16 21:35:14 +0300

    feat: add promo section

--- src/feature/main/promotion-content.tsx
+++ src/feature/main/promotion-content.tsx
@@ -4,19 +4,27 @@ import useSWR from 'swr';
 import { Spinner } from '@/components/ui/spiner';
 
 import fetchPromoCode from './api/fetch-promo-code';
+import handleCopyPromotionContent from './use-promotion-content';
 
 function PromotionContent() {
   const { data, error, isLoading } = useSWR<DiscountCode[], Error>('promo-codes', fetchPromoCode);
   if (isLoading) return <Spinner className="mx-auto" />;
   if (error) return <p>Error loading promo code.</p>;
-  if (!data || data.length === 0) return;
+  if (!data || data.length === 0) return 'Promocode not available';
 
   const { code, name } = data[0];
   return (
-    <>
-      <h2>Promo Code: {code}</h2>
+    <div className="flex w-full flex-row items-center gap-10 text-nowrap">
+      <h2>Click and get promocode! </h2>
+      <button
+        className="hover:text-chart-3 cursor-pointer font-bold"
+        onClick={() => void handleCopyPromotionContent(code)}
+        title="Click to copy"
+      >
+        {code}
+      </button>
       <p>Promotion: {name?.['en-GB'] ?? ''}</p>
-    </>
+    </div>
   );
 }
 
--- src/feature/main/promotion-section.tsx
+++ src/feature/main/promotion-section.tsx
@@ -2,8 +2,12 @@ import PromotionContent from './promotion-content';
 
 function PromotionSection() {
   return (
-    <section className="bg-chart-1 flex h-20 w-full flex-col items-center justify-center">
-      <PromotionContent />
+    <section className="flex h-20 w-full flex-row items-center justify-center gap-20">
+      <span className="content w-full border-b-2 text-white" />
+      <div>
+        <PromotionContent />
+      </div>
+      <span className="w-full border-b-2 text-white" />
     </section>
   );
 }
