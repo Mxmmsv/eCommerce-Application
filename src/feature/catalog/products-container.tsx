9e860bf46c1eff7bdda375515c5f8a82194968c1
ek-ole <ek_34@mail.ru>
2025-06-17 19:54:36 +0300

    feat: separate filters from the catalog

---
+++ src/feature/catalog/products-container.tsx
@@ -0,0 +1,65 @@
+import { useDeferredValue } from 'react';
+
+import { Spinner } from '@/components/ui/spinner';
+
+import { ProductList } from './product-list';
+import type { Poster } from './types';
+
+type ProductsContainerProps = {
+  products: { products: Poster[]; total: number } | undefined;
+  isLoading: boolean;
+  error: Error | undefined;
+  currentPage: number;
+  productsPerPage: number;
+  onPageChange: (page: number) => void;
+  searchQuery?: string;
+};
+
+export const ProductsContainer = ({
+  products,
+  isLoading,
+  error,
+  currentPage,
+  productsPerPage,
+  onPageChange,
+  searchQuery,
+}: ProductsContainerProps) => {
+  const deferredProducts = useDeferredValue(products);
+
+  if (isLoading) {
+    return (
+      <div className="column flex min-h-svh items-center justify-center">
+        <Spinner size="medium" className="text-primary">
+          <span className="text-center">Loading products...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex min-h-svh items-center justify-center">
+        <div className="text-center text-red-500">{error.message || 'Failed to load products'}</div>
+      </div>
+    );
+  }
+
+  if (!deferredProducts || deferredProducts.products.length === 0) {
+    return (
+      <div className="text-muted-foreground mt-4 text-center">
+        No products found {searchQuery ? `for "${searchQuery}"` : ''}.
+      </div>
+    );
+  }
+
+  return (
+    <ProductList
+      products={deferredProducts?.products || []}
+      totalProducts={deferredProducts?.total || 0}
+      currentPage={currentPage}
+      productsPerPage={productsPerPage}
+      onPageChange={onPageChange}
+      isLoading={isLoading}
+    />
+  );
+};
--- src/pages/catalog-page.tsx
+++ src/pages/catalog-page.tsx
@@ -1,15 +1,16 @@
 import { useSearchParams } from 'react-router';
 import useSWR from 'swr';
 
-import { Spinner } from '@/components/ui/spiner';
 import { config } from '@/config';
 import { fetchProducts } from '@/feature/catalog/api/fetch-products';
 import { Breadcrumbs } from '@/feature/catalog/categories/breadcrumbs';
 import { CategoryNavigation } from '@/feature/catalog/categories/category-navigation';
 import { useCategoryStore } from '@/feature/catalog/categories/use-category-store';
+import { DiscountFilter } from '@/feature/catalog/filter/discount-filter';
 import { TypeFilter } from '@/feature/catalog/filter/type-filter';
 import { useFilterStore } from '@/feature/catalog/filter/use-filter-store';
-import { ProductList } from '@/feature/catalog/product-list';
+import { ProductsContainer } from '@/feature/catalog/products-container';
+import { SortSelect } from '@/feature/catalog/sorting/sort-select';
 import { useSortStore } from '@/feature/catalog/sorting/use-sort-store';
 import type { Poster } from '@/feature/catalog/types';
 
@@ -61,24 +62,6 @@ export default function CatalogPage() {
       ),
   );
 
-  if (isLoading) {
-    return (
-      <div className="column flex min-h-svh items-center justify-center">
-        <Spinner size="medium" className="text-primary">
-          <span className="text-center">Loading products...</span>
-        </Spinner>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex min-h-svh items-center justify-center">
-        <div className="text-center text-red-500">{error.message || 'Failed to load products'}</div>
-      </div>
-    );
-  }
-
   return (
     <>
       <title>{'Catalog :: Poster store'}</title>
@@ -99,15 +82,17 @@ export default function CatalogPage() {
             </div>
           )}
 
-          {products?.products?.length === 0 && (
-            <div className="text-muted-foreground mt-4 text-center">
-              No products found {searchQuery ? `for "${searchQuery}"` : ''}.
+          <div className="flex justify-end">
+            <div className="mx-4 flex items-center justify-center">
+              <DiscountFilter />
             </div>
-          )}
+            <SortSelect />
+          </div>
 
-          <ProductList
-            products={products?.products || []}
-            totalProducts={products?.total || 0}
+          <ProductsContainer
+            products={products}
+            isLoading={isLoading}
+            error={error}
             currentPage={currentPage}
             productsPerPage={productsPerPage}
             onPageChange={onPageChange}
