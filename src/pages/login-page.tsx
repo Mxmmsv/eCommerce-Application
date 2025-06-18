d3376b2a71cefebf47bdcc2eb180a772dd900680
ek-ole <ek_34@mail.ru>
2025-06-18 16:50:41 +0300

    refactor: add wave to login page

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router';
 
 import AuthContext from '@/feature/auth/login/auth-provider';
 import { LoginForm } from '@/feature/auth/login/login-form';
+import SeparatorWave from '@/feature/main/separator-wave';
 
 export default function LoginPage() {
   const { IS_AUTHORIZED } = useContext(AuthContext);
@@ -14,10 +15,15 @@ export default function LoginPage() {
   return (
     <>
       <title>{'Login :: Poster store'}</title>
-      <div className="bg-muted flex min-h-[calc(100svh-20svh)] flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col gap-6">
-          <LoginForm />
+
+      <div className="bg-muted flex min-h-[calc(100svh-20svh)] flex-col justify-center">
+        <SeparatorWave top={true} color="fill-hero" />
+        <div className="bg-hero flex items-center justify-center gap-6 p-6 md:p-10">
+          <div className="flex w-full max-w-sm flex-col gap-6">
+            <LoginForm />
+          </div>
         </div>
+        <SeparatorWave color="fill-hero" />
       </div>
     </>
   );
